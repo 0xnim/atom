41b999265871530bea0626162275fa775f399861
Adam Walker <adam@perk.gg>
2025-11-09 20:42:56 +0000

    Remove `Blocks.kt` and extend block matching utilities in `Util.kt`
    - Deleted `Blocks.kt` to streamline block management approaches.
    - Added new utility methods to `Util.kt` for block matching with `Key` and string inputs.

--- src/main/kotlin/org/shotrush/atom/Util.kt
+++ src/main/kotlin/org/shotrush/atom/Util.kt
@@ -1,8 +1,10 @@
 package org.shotrush.atom
 
+import net.momirealms.craftengine.bukkit.api.CraftEngineBlocks
 import net.momirealms.craftengine.bukkit.api.CraftEngineItems
 import net.momirealms.craftengine.core.item.CustomItem
 import net.momirealms.craftengine.core.util.Key
+import org.bukkit.block.Block
 import org.bukkit.inventory.ItemStack
 import org.shotrush.atom.item.isItem
 
@@ -10,3 +12,7 @@ fun ItemStack.matches(key: Key) = CraftEngineItems.getCustomItemId(this) == key
 fun ItemStack.matches(key: String) = matches(Key.of(key))
 fun ItemStack.matches(namespace: String, path: String) = matches(Key.of(namespace, path))
 fun ItemStack.matches(item: CustomItem<ItemStack>) = item.isItem(this)
+
+fun Block.matches(key: Key) = CraftEngineBlocks.getCustomBlockState(this)?.owner()?.matchesKey(key) ?: false
+fun Block.matches(key: String) = matches(Key.of(key))
+fun Block.matches(namespace: String, path: String) = matches(Key.of(namespace, path))
